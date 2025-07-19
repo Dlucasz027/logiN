00dadae7f00f0c9fcfcef104642c886390cf8521
Dlucasz027 <matheuslucas.027@gmail.com~>
2025-07-19 18:13:17 -0300

    Implementa sistema de login e cadastro de usuários
    - Criação das views para cadastro, login e logout
    - Configuração das URLs para autenticação
    - Aplicação de CSS estilizado nas páginas de login e cadastro
    - Ajustes no painel admin para melhor gerenciamento
    - Mensagens de feedback para usuário nas telas de autenticação

--- logiN/admin.py
+++ logiN/admin.py
@@ -1,4 +1,12 @@
 from django.contrib import admin
-from .models import Fotografia
+from logiN.models import Fotografia
 
-admin.site.register(Fotografia) #Itens vem do model criado
+class ListandoFotografias(admin.ModelAdmin):
+    list_display = ("id", "nome", "legenda", "publicada")
+    list_display_links = ("id","nome")
+    search_fields = ("nome",)
+    list_filter = ("categoria", 'usuario')
+    list_editable = ("publicada",)
+    list_per_page = 10
+
+admin.site.register(Fotografia, ListandoFotografias)
