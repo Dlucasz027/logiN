e5a9d5ecc1241782f67cd10673f339a1ed05e08d
Dlucasz027 <matheuslucas.027@gmail.com~>
2025-07-31 19:28:22 -0300

    docs: adicionar comentários de documentação em apps.py, urls.py e views.py do app principal

--- logiN/urls.py
+++ logiN/urls.py
@@ -1,7 +1,7 @@
-from django.urls import path
-from logiN.views import index, exclusiva
+from django.urls import path # Importa o módulo path responsável por definir URLs | Import the path module responsible for defining URLs
+from logiN.views import index, exclusiva  # Importa as views index e exclusiva do módulo views | Import the index and exclusiva views from the views module
 
-urlpatterns = [
-    path('', index, name='index'),  # 👈 aqui define que a raiz ('') será atendida pela view index
-    path('exclusiva/', exclusiva, name='exclusiva'), #area depois de fazer login, exclusiva
+urlpatterns = [  # Lista especial do Django que contém as URLs do aplicativo | Special Django list that contains the app's URLs
+    path('', index, name='index'),  
+    path('exclusiva/', exclusiva, name='exclusiva'), # exclusiva/ representa a URL para acessar a página, exclusiva é importada das views e por último o nome que será usado para referenciar essa URL | 'exclusiva/' represents the URL to access the page, exclusiva is imported from views, and finally the name that will be used to reference this URL
 ]
