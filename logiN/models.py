3729f9ae693f3f058e3dfb3a075164fe964276b4
Dlucasz027 <matheuslucas.027@gmail.com~>
2025-08-18 21:20:31 -0300

    docs(models): adicionar comentários explicativos ao model Fotografia

--- logiN/models.py
+++ logiN/models.py
@@ -1,16 +1,14 @@
-from django.db import models
-from datetime import datetime
-from django.contrib.auth.models import User
+from django.db import models #Módulo que cira tabelas no banco de dados | Module that creates tables in the database
+from datetime import datetime #Import para manipulação de data e hora | Import for date and time manipulation
+from django.contrib.auth.models import User #Modelo padrão de usuários para relacionar as imagens upadas ao próprio user | Default user model to relate uploaded images to the user
 
-class Fotografia(models.Model):
+class Fotografia(models.Model): #Classe com models.model vira tabela no banco de dados | Class with models.Model creates a table in the database
 
     OPCOES_CATEGORIA = [
-        ("NEBULOSA","Nebulosa"),
-        ("ESTRELA","Estrela"),
-        ("GALÁXIA","Galáxia"),
-        ("PLANETA","Planeta"),
+        ("FOTOGRAFIA","Fotografia"), #Categoria das imagens inseridas | Category of the inserted images
     ]
 
+#Campos da tabela | Fields of the table
     nome = models.CharField(max_length=100, null=False, blank=False)
     legenda = models.CharField(max_length=150, null=False, blank=False)
     categoria = models.CharField(max_length=100, choices=OPCOES_CATEGORIA, default='')
@@ -26,5 +24,5 @@ class Fotografia(models.Model):
         related_name='user'
     )
 
-    def __str__(self):
+    def __str__(self): #Método especial para retornar o nome da imagem conforme definido no campo nome | Special method to return the name of the image as defined in the name field
         return self.nome
